59da4da053c223a8aa7993aeba0bceee55845110
Emilse Arias <emilsearias@gmail.com>
2022-08-02 11:00:52 -0300

    agregamos el state tarea, arregloTareas y lo enviamos por props al componente ListaTareas

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -4,25 +4,46 @@ import React, {useState} from "react";
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
 
+
 const Formulario = () => {
 //aqui va la logica
 //crear un state
 const [arregloTareas, setArregloTareas] = useState([]);
 const [tarea, setTarea] = useState('');
 
+// const actualizarTarea = (e) =>{
+//   //obtener el value de mi input
+//   // console.log(e.target.value);
+//   //actualizar el state
+//   setTarea(e.target.value.trim());
+// }
+
+const handleSubmit = (e) =>{
+  e.preventDefault();
+  console.log('prueba desde submit');
+  //actualizar la lista de tareas
+  setArregloTareas([...arregloTareas, tarea]);
+  setTarea('');
+}
+
 //aqui va el maquetado y un poquito de logica
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
+          <Form.Control 
+          type="text" 
+          placeholder="Ingrese una tarea" 
+          onChange={(e)=> setTarea(e.target.value.trim())}
+          value={tarea}
+          />
           <Button variant="primary" type="submit">
             Enviar
           </Button>
         </Form.Group>
       </Form>
       {/* aqui invoco a la lista de tareas */}
-      <ListaTareas></ListaTareas>
+      <ListaTareas arregloTareas={arregloTareas}></ListaTareas>
     </div>
   );
 };
